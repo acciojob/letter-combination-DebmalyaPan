084490a6eed4c322ecc7e47bab958ee670bbe980
DebmalyaPan <112159960+DebmalyaPan@users.noreply.github.com>
2023-01-25 22:21:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 function letterCombinations(digit) {
-	//   let arr = {
-	// 	  0 : 0,
-	// 	  1 : 1,
-	// 	  2 : {a,b,c},
-	// 	  3 : {d,e,f},
-	// 	  4 : {g,h,i},
-	// 	  5 : {j,k,l},
-	// 	  6 : {m,n,o},
-	// 	  7 : {p,q,r,s},
-	// 	  8 : {t,u,v},
-	// 	  9 : {w,x,y,z}
-	//   }
- //  let n = input_digit.length;
-	// for(let i=0;i<n;i++){
-		
-	// }
 	   if (!digits) {
         return [];
     }
