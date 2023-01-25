92b677e34f058394d24e9a794befab38cda9c9eb
DebmalyaPan <112159960+DebmalyaPan@users.noreply.github.com>
2023-01-25 22:30:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-function letterCombinations(digit) {
-	   if (!digits) {
+function letterCombinations(input_digit) {
+  if(input_digit == ""){
         return [];
     }
-    let map = {
-        2: "abc",
-        3: "def",
-        4: "ghi",
-        5: "jkl",
-        6: "mno",
-        7: "pqrs",
-        8: "tuv",
-        9: "wxyz"
-    };
-    let res = [""];
-    for (let i = 0; i < digits.length; i++) {
-        let tmp = [];
-        for (let j = 0; j < res.length; j++) {
-            for (let k = 0; k < map[digits[i]].length; k++) {
-                tmp.push(res[j] + map[digits[i]][k]);
+    let table = [ '0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+     
+    let res =[];
+    let que = [''];
+     
+    while(que.length>0){
+        let str = que[0];
+        que.shift();
+         
+        if(str.length == input_digit.length){
+            res.push(str); // if all input_digit are replaced with char push to result
+        } else{
+//             get the current number from the input_digit i.e if str.length = 2 , input_digit =123 s= 3
+            let s= Number(input_digit.charAt(str.length));
+            let val = table[s]; // get char from the table i.e def for s =3
+             
+            for(i=0;i<val.length;i++){
+                que.push(str+val.charAt(i));
             }
         }
-        res = tmp;
     }
-    return res.sort();
+     
+    return res;
+     
 }
- 
+
 module.exports = letterCombinations;
